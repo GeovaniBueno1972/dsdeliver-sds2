476d92df1338795e238fb885fcc388238281bd86
Geovani Bueno <geovani.bueno@yahoo.com.br>
2021-01-13 20:31:22 -0300

    Finish front-web

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
-import { fetchProducts } from '../api';
+import { toast } from 'react-toastify';
+import { fetchProducts, saveOrder } from '../api';
+import Footer from '../Footer';
+import { checkisSelected } from './Helpers';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
@@ -9,7 +13,11 @@ import { OrderLocationData, Product } from './types';
 function Orders(){
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderlocation, setOrderlocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+      return sum + item.price;
+    }, 0);
 
     useEffect(() => {
         fetchProducts()
@@ -18,12 +26,51 @@ function Orders(){
 
     }, []);
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkisSelected(selectedProducts, product);
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
+    const handleSubmit = () => {
+      const productsIds = selectedProducts.map(({id}) => ({id}));
+      const payload = {
+        ...orderlocation!, 
+        products: productsIds
+      }
+
+      saveOrder(payload).then((response) => {
+        toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+        setSelectedProducts([]);
+      })
+        .catch(() => {
+          toast.warning('Erro ao enviar pedido');
+        })
+    }
+
     return (
-       <div className="orders-container">
+       <>
+        <div className="orders-container">
            <StepsHeader />
-           <ProductsList products={products}/>
+           <ProductsList 
+           products={products}
+           onSelectProduct={handleSelectProduct}
+           selectedProducts={selectedProducts}
+           />
            <OrderLocation onChangeLocation={location => setOrderlocation(location)} />
-       </div>
+           <OrderSummary 
+           amount={selectedProducts.length} 
+           totalPrice={totalPrice}
+           onSubmit = {handleSubmit}
+           />
+        </div>
+        <Footer />
+       </>
     )
 }
 
